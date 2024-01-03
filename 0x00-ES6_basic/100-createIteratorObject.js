50f53789eb3e4ceeae530ab2d936dac6f556c4a4
salmalah <122815714+salmalah@users.noreply.github.com>
2024-01-03 19:33:58 +0100

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,41 +1,12 @@
 // 100-createIteratorObject.js
 
-export default function createIteratorObject(report) {
-  const iterator = {
-    currentDepartmentIndex: 0,
-    currentEmployeeIndex: 0,
-    next() {
-      const departments = Object.keys(report);
-
-      // Check if there are more departments
-      if (this.currentDepartmentIndex < departments.length) {
-        const currentDepartment = departments[this.currentDepartmentIndex];
-        const employees = report[currentDepartment];
-
-        // Check if there are more employees in the current department
-        if (this.currentEmployeeIndex < employees.length) {
-          const currentEmployee = employees[this.currentEmployeeIndex];
-          this.currentEmployeeIndex++;
-
-          return { value: currentEmployee, done: false };
-        }
-
-        // Move to the next department
-        this.currentDepartmentIndex++;
-        this.currentEmployeeIndex = 0;
-
-        // Recursive call to handle the next department
-        return this.next();
+export default function* createIteratorObject(report) {
+  for (const departmentName in report.allEmployees) {
+    if (Object.prototype.hasOwnProperty.call(report.allEmployees, departmentName)) {
+      const employeesList = report.allEmployees[departmentName];
+      for (const employeeName of employeesList) {
+        yield employeeName;
       }
-
-      // No more departments and employees
-      return { done: true };
-    },
-  };
-
-  iterator[Symbol.iterator] = function () {
-    return this;
-  };
-
-  return iterator;
+    }
+  }
 }
