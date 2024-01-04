7a700e7ce52912e42cb791868df63b75d467fce3
salmalah <122815714+salmalah@users.noreply.github.com>
2024-01-04 16:06:18 +0100

    Update 1-make_classrooms.js

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,11 +1,6 @@
 // Import the ClassRoom class from 0-classroom.js
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
-function initializeRooms() {
-  const sizes = [19, 20, 34];
-
-  const rooms = sizes.map((size) => new ClassRoom(size));
-
-  return rooms;
+export default function initializeRooms() {
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
-export default initializeRooms;
