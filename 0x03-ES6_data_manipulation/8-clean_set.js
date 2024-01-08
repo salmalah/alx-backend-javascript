9fa9c80f0374f39a60d92ef23c2974d2383a60b2
salmalah <s.salmalahmidi@gmail.com>
2024-01-08 21:46:41 +0000

    update

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,10 @@
+// 8-clean_set.js
 export default function cleanSet(set, startString) {
-  const filteredValues = [...set].filter((value) => value.startsWith(startString));
-  const resultString = filteredValues.join('-');
-  return resultString;
+  // Filter the set values that start with the specified string
+  const filteredValues = [...set].filter(value => value.startsWith(startString));
+
+  // Strip the startString from each value
+  const strippedValues = filteredValues.map(value => value.slice(startString.length));
+
+  return strippedValues.join('-');
 }
