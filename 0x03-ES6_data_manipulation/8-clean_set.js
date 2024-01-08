f16597c36c882d31f03d9eab777501f1b6401d3a
salmalah <122815714+salmalah@users.noreply.github.com>
2024-01-08 22:35:08 +0100

    Create 8-clean_set.js

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,5 @@
+export default function cleanSet(set, startString) {
+  const filteredValues = [...set].filter((value) => value.startsWith(startString));
+  const resultString = filteredValues.join("-");
+  return resultString;
+}
