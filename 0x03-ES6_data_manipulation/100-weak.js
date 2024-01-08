ea801e12e39f01c979dfeec8b9e98adf1070b33d
salmalah <122815714+salmalah@users.noreply.github.com>
2024-01-08 22:55:16 +0100

    Create 100-weak.js

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,18 @@
+// 100-weak.js
+
+const weakMap = new WeakMap();
+
+export { weakMap };
+
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 1);
+  } else {
+    const count = weakMap.get(endpoint) + 1;
+    weakMap.set(endpoint, count);
+
+    if (count >= 5) {
+      throw new Error('Endpoint load is high');
+    }
+  }
+}
