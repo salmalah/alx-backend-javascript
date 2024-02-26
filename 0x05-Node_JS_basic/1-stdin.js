e744487d5a48479068a02363a87a20a2235b9b9d
salmalah <122815714+salmalah@users.noreply.github.com>
2024-02-26 17:47:51 +0100

    Update 1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,17 +1,10 @@
+/* eslint */
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.setEncoding('utf8');
-
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
-
-process.stdin.on('data', function(data) {
-  const input = data.trim();
-  if (input !== '') {
-    process.stdout.write(`Your name is: ${input}\n`);
-    process.stdout.write("This important software is now closing\n");
-    process.exit();
-  }
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) process.stdout.write(`Your name is: ${name}`);
 });
-
-process.stdin.on('end', function() {
-  process.stdout.write("This important software is now closing\n");
-  process.exit();
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
