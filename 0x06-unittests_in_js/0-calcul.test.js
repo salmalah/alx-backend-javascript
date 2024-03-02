973ea3f03a26f57aa5b2511661403dae25908817
salmalah <122815714+salmalah@users.noreply.github.com>
2024-03-02 20:04:50 +0100

    Update 0-calcul.test.js

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,15 +2,16 @@ const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', function() {
-  it('should return the sum of rounded numbers', function() {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  describe('sum of rounded numbers', function() {
+    it('should return the sum of rounded numbers', function() {
+      assert.strictEqual(calculateNumber(1, 3), 4);
+      assert.strictEqual(calculateNumber(1, 3.7), 5);
+      assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+      assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    });
   });
-});
 
-describe('second number rounded', function() {
+  describe('second number rounded', function() {
     it('should round the second number and return the sum', function() {
       assert.strictEqual(calculateNumber(1.1, 3), 4);
       assert.strictEqual(calculateNumber(1.9, 3), 5);
