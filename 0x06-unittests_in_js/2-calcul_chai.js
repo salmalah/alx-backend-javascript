0afc11df2df03e695b4b6c04c33186f565ffa257
salmalah <122815714+salmalah@users.noreply.github.com>
2024-03-02 20:13:55 +0100

    Update 2-calcul_chai.js

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,5 +1,14 @@
-function calculateNumber(a, b) {
-  return Math.round(a) + Math.round(b);
-}
+const calculateNumber = (type, a, b) => {
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
+  }
+  if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  }
+  if (type === 'DIVIDE') {
+    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+  }
+  return 0;
+};
 
 module.exports = calculateNumber;
