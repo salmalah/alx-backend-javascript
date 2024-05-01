9bdedbf0324e4484d60c9c45af8a1748abb93f75
salmalah <122815714+salmalah@users.noreply.github.com>
2024-05-01 18:18:57 +0100

    Update 2-calcul_chai.test.js

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,22 +1,30 @@
 const chai = require('chai');
+const { expect } = chai;
+const { it, describe } = require('mocha');
 const calculateNumber = require('./2-calcul_chai');
 
-const expect = chai.expect;
-
-describe('calculateNumber', function() {
-  describe('sum of rounded numbers', function() {
-    it('should return the sum of rounded numbers', function() {
-      expect(calculateNumber(1, 3)).to.equal(4);
-      expect(calculateNumber(1, 3.7)).to.equal(5);
-      expect(calculateNumber(1.2, 3.7)).to.equal(5);
-      expect(calculateNumber(1.5, 3.7)).to.equal(6);
-    });
+describe('calculateNumber', () => {
+  it('checking if sum operation is correct', () => {
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
   });
 
-  describe('second number rounded', function() {
-    it('should round the second number and return the sum', function() {
-      expect(calculateNumber(1.1, 3)).to.equal(4);
-      expect(calculateNumber(1.9, 3)).to.equal(5);
-    });
+  it('checking if subtract operation is correct', () => {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
+  });
+
+  it('checking if division operation is correct', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.equal(0.2);
+  });
+
+  it('checking if division operation is doable', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
+  });
+
+  it('checking correct type for operation 1', () => {
+    expect(calculateNumber(5, 1, 4)).to.equal(0);
+  });
+
+  it('checking correct type for operation 2', () => {
+    expect(calculateNumber('plus', 1, 4)).to.equal(0);
   });
 });
